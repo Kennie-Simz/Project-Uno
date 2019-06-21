f2a774b370edab7629779e8b1e3cd144dfa1c25d
Kennedy Simiyu <nyongesa17@outlook.com>
2019-06-21 15:54:35 +0300

    Adjusting files

--- login.js
+++
@@ -1,7 +0,0 @@
-$('form.signinup').on("click", '.to-signup', function() {
-	$(this).parents('form.signinup').addClass('is-signup');
-});
-
-$('form.signinup').on("click", '.to-signin', function() {
-	$(this).parents('form.signinup').removeClass('is-signup');
-});
