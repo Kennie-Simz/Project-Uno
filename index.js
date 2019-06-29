414267ba9f55a2b07f350ccabd9d5b9c54d9b197
Kennedy Simiyu <nyongesa17@outlook.com>
2019-06-29 19:25:21 +0300

    [Finishes #166997214] Linting is set!

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
-const port = process.env.port || 3000
+const express = require('express');
+
+const app = express();
+const port = process.env.port || 3000;
 
 
-app.get('/', (req, res) => res.send('Hello World, We are live!'))
+app.get('/', (req, res) => res.send('Hello World, We are live!'));
 
-app.listen(port, () => console.log(`We are live at port ${port}`))
+app.listen(port, () => console.log(`We are live at port ${port}`));
