387a8caedda11c2c12efef3e0ea56801963e56a6
Kennedy Simiyu <nyongesa17@outlook.com>
2019-07-09 18:02:28 +0300

    routing in place

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,27 +10,29 @@ class authController {
 
   static createUser(req, res) {
     const newId = parseInt(Users.length, 10) + 1;
-    const { email, first_name, last_name, password, phoneNumber, address} = req.body;
+    const {
+      email, firstName, lastName, password, phoneNumber, address,
+    } = req.body;
     const newUser = {
       id: newId,
       email,
-      first_name,
-      last_name,
+      firstName,
+      lastName,
       password,
       phoneNumber,
       address,
-      is_admin: false
+      is_admin: false,
     };
     Users.push(newUser);
     return res.status(200).json({
       status: 'success',
       data: {
-        token: "hgahghjad",
+        token: 'hgahghjad',
         id: newId,
-        first_name,
-        last_name,
-        email
-      }
+        firstName,
+        lastName,
+        email,
+      },
     });
   }
 }
