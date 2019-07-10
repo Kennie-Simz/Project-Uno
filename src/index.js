ad65c3984f079ecb4bedad9e34118f13e3f57f45
Kennedy Simiyu <nyongesa17@outlook.com>
2019-07-10 15:22:12 +0300

    [Finishes #167195497] Set up json web tokens

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import propertyRouter from './routes/property';
 import { APP_PORT } from './config';
 
 const app = express();
+app.use(
+    bodyParser.urlencoded({
+        // Middleware
+      extended: true,
+    })
+  );
 app.use(bodyParser.json());
 
 app.use('/api/v1/auth', userRouter);
 app.use('/api/v1/property', propertyRouter);
 
 app.listen(APP_PORT, () => console.log(`App listening on port ${APP_PORT}!`));
+
+export default app;
