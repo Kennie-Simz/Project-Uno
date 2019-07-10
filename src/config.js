ff10f369c6179df978b69bb56fd9654361495fcd
Kennedy Simiyu <nyongesa17@outlook.com>
2019-07-10 14:48:14 +0300

    [Finishes #167194996]Set up dotenv files

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const { 
+    APP_PORT = 4000,
+    APP_SECRET = 'thisistheappsecret'
+
+} = process.env;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import userRouter from './routes/auth';
 import propertyRouter from './routes/property';
+import { APP_PORT } from './config';
 
 const app = express();
 app.use(bodyParser.json());
 
-const PORT = 4000;
-
 app.use('/api/v1/auth', userRouter);
 app.use('/api/v1/property', propertyRouter);
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+app.listen(APP_PORT, () => console.log(`App listening on port ${APP_PORT}!`));
