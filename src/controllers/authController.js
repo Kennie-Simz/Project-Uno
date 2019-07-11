f595e665d0972394293851ebff72dbbf3de8bb17
Kennedy Simiyu <nyongesa17@outlook.com>
2019-07-11 15:05:55 +0300

    [Finishes #167221792] A user can login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,6 +11,41 @@ class authController {
     });
   }
 
+  static logUsers(req, res, next) {
+    const {email, password } = req.body;
+    const logUser = Users.find(function (item) { return item.email === email; });
+    if(logUser){
+      if(logUser.password === password){
+        const token = jwt.sign({ id: Users.id }, APP_SECRET, {
+          expiresIn: '24h', // expires in 24 hours
+        });
+        res.json({
+          status: 'success',
+          data: {
+            token,
+            id: logUser.id,
+            firstName: logUser.firstName,
+            lastName: logUser.lastName,
+            email: logUser.email,
+          }
+        }) 
+      }else{
+
+        res.json({
+          status: 'error',
+          error: 'Password is incorrect',
+          inputedPassword: password,
+          dbPassword: Users.password
+        })
+      }
+    }else{
+      res.json({
+        status: 'error',
+        error: 'Email does not exist' 
+      })
+    }
+  }
+  
   static createUser(req, res) {
     const newId = parseInt(Users.length, 10) + 1;
     const {
@@ -28,7 +63,7 @@ class authController {
     };
     Users.push(newUser);
     const token = jwt.sign({ id: newId }, APP_SECRET, {
-      expiresIn: "24h" // expires in 24 hours
+      expiresIn: '24h', // expires in 24 hours
     });
     return res.status(200).json({
       message: 'success',
--- test/auth.test.spec.js
+++ test/auth.test.spec.js
@@ -1,19 +1,56 @@
-import chai from "chai";
-import chaiHttp from "chai-http";
-import app from "../src/index";
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/index';
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
-describe("Auth", () => {
-  describe("Get all users /", () => {
-    // Test to get all students record
-    it("should get all users", done => {
+describe('Auth', () => {
+  describe('Get all users /', () => {
+    it('should get all users', (done) => {
       chai
         .request(app)
-        .get("/api/v1/auth/signup")
+        .get('/api/v1/auth/signup')
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.be.a("object");
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+  describe('Signup a user', () => {
+    it('should signup a new user', (done) => {
+      let newUser ={
+        email: "jondoe@cru.com",
+        firstName: "Jason",
+        lastName: "Ashu",
+        password: "newpassword",
+        phoneNumber: "0706958423",
+        address: "12th Mpale"
+      };
+      chai
+        .request(app)
+        .post('/api/v1/auth/signup')
+        .send(newUser)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+  describe('Sign in a user', () => {
+    it('should sign in a user', (done) => {
+      let newUser ={
+        email: "jondoe@cru.com",
+        password: "newpassword"
+      };
+      chai
+        .request(app)
+        .post('/api/v1/auth/signin')
+        .send(newUser)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
           done();
         });
     });
