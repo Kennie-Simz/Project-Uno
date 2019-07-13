6b62aff9f04e19a9a5d7d4ca8f8b861554ab44ca
Kennedy Simiyu <nyongesa17@outlook.com>
2019-07-13 09:52:14 +0300

    switched ports

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node nodemon --exec babel-node bin/dev
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const {
-  APP_PORT = 4000,
+  APP_PORT = process.env.APP_PORT || 4000,
   APP_SECRET = 'thisistheappsecret',
 
 } = process.env;
